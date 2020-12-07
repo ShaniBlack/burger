a75c2cffd359c6755cebf5788def87e96a40b789
Shani Black <shaniblack2004@gmail.com>
2020-12-06 22:02:43 -0600

    created server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express"),
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// serve static content for the app from the "public" directory in the application directory
+app.use(express.static("public"));
+
+// parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require ("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT " + PORT)
+});
