8fb88a9a2052e6c0a418e57efed4447ad16e1786
Shani Black <shaniblack2004@gmail.com>
2020-12-06 22:47:20 -0600

    created the functions that will interact with the database

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,24 @@
 const orm = require("./config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input for the ORM
+let burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
 
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+}
 
 module.exports = burger
