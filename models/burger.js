97e7b112ef486870da7f7123ed9042195a27cf46
Shani Black <shaniblack2004@gmail.com>
2020-12-06 23:52:29 -0600

    added a touch of css styling

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,6 @@
 const orm = require("./config/orm.js");
 
-// create the code that will call the ORM functions using burger specific input for the ORM
+// created the code that will call the ORM functions using burger specific input for the ORM
 let burger = {
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
