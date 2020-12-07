8ff84a1dbbb8333734faafb88477ef4afb054021
Shani Black <shaniblack2004@gmail.com>
2020-12-06 22:59:10 -0600

    created all routes and set up logic within routes where required

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,6 +1,45 @@
-const express = require("express")
+const express = require("express");
+const router = express.Router();
+
+// import the model(burger.js) to use it's db functions
 const burger = require("./models/burger.js");
 
 // create 'router' for the app
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        let hbsObj = {
+            burgers: data
+        };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertID});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router
