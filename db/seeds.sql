87d25b30b585f3d3d88a12097b3641a389277d69
Shani Black <shaniblack2004@gmail.com>
2020-12-06 20:07:57 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Hamburger", true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("American Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Swiss Burger", true)
