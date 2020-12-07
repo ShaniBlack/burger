9930ded8cd55ecfd9b9c26a9c92cd8e8cb7484a6
Shani Black <shaniblack2004@gmail.com>
2020-12-06 22:40:22 -0600

    created main.handlebars to be used by handlebars/added button in index

--- config/orm.js
+++ config/orm.js
@@ -33,7 +33,7 @@ function objToSql(ob) {
 }
 
 let orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
         let queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
@@ -43,7 +43,7 @@ let orm = {
         });
     },
 
-        create: function(table, cols, vals, cb) {
+        insertOne: function(table, cols, vals, cb) {
             let queryString = "INSERT INTO " + table;
         
             queryString += " (";
@@ -64,7 +64,7 @@ let orm = {
             });
           },
     
-    update: function(table, objColVals, condition, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
     
         queryString += " SET ";
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,6 @@
 const orm = require("./config/orm.js");
 
+// create the code that will call the ORM functions using burger specific input for the ORM
 
 
-module.exports = burger.js
+module.exports = burger
